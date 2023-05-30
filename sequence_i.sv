7344cc7cd80294e1a23790b0abddacbd8f37bdc1
Aniket Shukla <aniket14042001@gmail.com>
2023-05-30 05:44:06 +0000

    new files added

---
+++ sequence_i.sv
@@ -0,0 +1,11 @@
+class sequence_i extends uvm_sequence_item;
+
+/* declaration of the item*/
+
+    function new(string name="sequence_i",uvm_component parent=null);
+
+        super.new(name,parent);
+
+    endfunction
+
+endclass
